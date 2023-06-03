22d73111fba475dff902f99a52f8c9af5370c68f
IvanovaAlina <irinap.1984@mail.ru>
2023-06-03 18:32:08 +0300

    set1

--- test_task/task_app/forms.py
+++ test_task/task_app/forms.py
@@ -5,7 +5,7 @@ class SalaryForm(forms.ModelForm):
     class Meta:
         model = InformSalary
         fields = ['date_check', 'summ_check', 'date_next_raise']
-        labels = {'date_check': 'Дата назначения зп','summ_check' :'Сумма зараплаты', 'date_next_raise': 'Дата следующего повышения'}
+        labels = {'date_check': 'Дата назначения зп','summ_check' :'Сумма зарплаты', 'date_next_raise': 'Дата следующего повышения'}
 
 
 
